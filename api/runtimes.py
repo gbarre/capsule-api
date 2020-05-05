060d7d12243b0e46ee2c6048da94f601b6aca89f
François Lafont <francois.lafont@ac-versailles.fr>
2020-05-05 18:53:47 +0200

    [wip] runtimes implementation

--- api/runtimes.py
+++ api/runtimes.py
@@ -1,14 +1,26 @@
+from app import db, oidc
+
+# GET /runtimes
+@oidc.accept_token(require_token=True, render_errors=False)
 def search(offset, limit, filters):
     pass
 
+# GET /runtimes/{rID}
+@oidc.accept_token(require_token=True, render_errors=False)
 def get(runtime_id):
     pass
 
+# POST /runtimes
+@oidc.accept_token(require_token=True, render_errors=False)
 def post(runtime):
     pass
 
+# PUT /runtimes/{rId}
+@oidc.accept_token(require_token=True, render_errors=False)
 def put(runtime_id, runtime):
     pass
 
+# DELETE /runtimes/{rId}
+@oidc.accept_token(require_token=True, render_errors=False)
 def delete(runtime_id):
     pass
---
+++ tests/api/test_runtimes.py
@@ -0,0 +1,100 @@
+from app import oidc
+from exceptions import KeycloakUserNotFound
+from tests.utils import dict_contains
+from unittest.mock import patch
+import tests.foodata as foodata
+
+class TestRuntimes:
+    _runtime_input = {
+        "name": "Runtime Test",
+        "runtimeType": "webapp"
+    }
+
+    _runtime_output = foodata.runtime1
+
+    #################################
+    #### Testing GET /runtimes
+    #################################
+    # Response 200:
+    def test_get(self, testapp, db, monkeypatch):
+        with patch.object(oidc, 'validate_token', return_value=True):
+            res = testapp.get('v1/runtimes', status=200).json
+            assert dict_contains(res[0], self._runtime_ouput)
+
+    # Response 401:
+    def test_get_with_no_token(self, testapp):
+        testapp.get('/v1/runtimes', status=401)
+    #################################
+
+    #################################
+    #### Testing POST /runtimes
+    #################################
+    # Response 201:
+    # def test_create(self, testapp, db, monkeypatch):
+    #     with patch.object(oidc, 'validate_token', return_value=True):
+    #         res = testapp.post_json('/v1/runtimes', self._runtime_input, status=201).json
+    #         assert dict_contains(res, self._runtime_input)
+
+    # Response 400:
+    # TODO: bad json input (missing name or runtimeType)
+
+    # Response 401:
+    # def test_create_with_no_token(self, testapp):
+    #     testapp.post_json('/v1/runtimes', self._runtime_input, status=401)
+
+    # Response 403:
+    # def test_create_with_non_admin(self, testapp, db, monkeypatch):
+
+    #################################
+
+    # TODO: GET ; PUT & DELETE runtimes/rId
+    #################################
+    #### Testing GET /runtimes/rId
+    #################################
+    # Response 200:
+
+
+    # Response 401:
+
+
+    # Response 200:
+
+
+    #################################
+
+    #################################
+    #### Testing PUT /runtimes/rId
+    #################################
+    # Response 200:
+
+
+    # Response 201:
+
+
+    # Response 400:
+
+
+    # Response 401:
+
+
+    # Response 403:
+
+
+    #################################
+
+    #################################
+    #### Testing DELETE /runtimes/rId
+    #################################
+    # Response 204:
+
+
+    # Response 400:
+
+
+    # Response 401:
+
+
+    # Response 403:
+
+
+    #################################
