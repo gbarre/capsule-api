e7abcef8b3a0aec460ceb6a8a9dd08a0c370e624
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2020-05-26 18:16:57 +0200

    WIP: implement capsules/{cId}/owners
    Co-authored-by: François Lafont <francois.lafont@ac-versailles.fr>

--- api/capsules/owners.py
+++ api/capsules/owners.py
@@ -1,19 +1,101 @@
+from flask import request
 from models import RoleEnum
+from models import Capsule, User, user_schema
 from app import db, oidc
-from utils import oidc_require_role
+from utils import oidc_require_role, check_owners_on_keycloak
+from werkzeug.exceptions import NotFound, BadRequest, Forbidden, Conflict
+from exceptions import KeycloakUserNotFound
 
 
 # /GET /capsules/{cId}/owners
-@oidc.accept_token(require_token=True, render_errors=False)
-def search(offset, limit, filters):
-    pass
+@oidc_require_role(min_role=RoleEnum.user)
+def search(capsule_id, offset, limit, filters, user):
+    try:
+        capsule = Capsule.query.get(capsule_id)
+    except:
+        raise BadRequest
+
+    if capsule is None:
+        raise NotFound(description=f"The requested capsule '{capsule_id}' has not been found.")
+
+    owners = []
+    user_is_owner = False
+    for owner in capsule.owners:
+        if user.name == owner.name:
+            user_is_owner = True
+        user_json = user_schema.dump(owner).data
+        owners.append(user_json)
+
+    if not user_is_owner:
+        raise Forbidden("You don't have the permission to access the requested resource.")
+
+    return owners
+
 
 # /PATCH /capsules/{cId}/owners
-@oidc.accept_token(require_token=True, render_errors=False)
-def patch(user_id, user):
-    pass
+@oidc_require_role(min_role=RoleEnum.user)
+def patch(capsule_id, user):
+    try:
+        capsule = Capsule.query.get(capsule_id)
+    except:
+        raise BadRequest
+
+    if capsule is None:
+        raise NotFound(description=f"The requested capsule '{capsule_id}' has not been found.")
+
+    owner_data = request.get_json()
+
+    if "newOwner" not in owner_data:
+        raise BadRequest("The key newOwner is required.")
+    new_owner = owner_data["newOwner"]
+
+    user_is_owner = False
+    for owner in capsule.owners:
+        if user.name == owner.name:
+            user_is_owner = True
+        if new_owner == owner.name:
+            raise Conflict
+
+    if not user_is_owner:
+        raise Forbidden("You don't have the permission to access the requested resource.")
+
+########################################################################################################################
+
+    try:  # Check if owners exist on Keycloak
+        check_owners_on_keycloak([new_owner])
+    except KeycloakUserNotFound as e:
+        raise NotFound(description=f'{e.missing_username} is an invalid user.')
+
+    # Get existent users, create the others
+    user = User.query.filter_by(name=new_owner).one_or_none()
+    if user is None:  # User does not exist in DB
+        new_user = User(name=owner, role=RoleEnum.user)
+    else:
+        new_user = user
+
+    # TODO : update capsule with new owner
+
+    # capsule.owners.append(new_user)
+
+    # db.session.add(capsule)
+    # db.session.commit()
+
+    # result = Capsule.query.get(capsule.id)
+    # return capsule_schema.dump(result).data, 201, {
+    #     'Location': f'{request.base_url}/{capsule.id}',
+    # }
+################################
+
+
+
 
 # /DELETE /capsules/{cId}/owners/{uId}
-@oidc.accept_token(require_token=True, render_errors=False)
-def delete(user):
-    pass
+@oidc_require_role(min_role=RoleEnum.user)
+def delete(capsule_id, user_id, user):
+    try:
+        capsule = Capsule.query.get(capsule_id)
+    except:
+        raise BadRequest
+
+    if capsule is None:
+        raise NotFound(description=f"The requested capsule '{capsule_id}' has not been found.")
---
+++ tests/api/test_capsule_owners.py
@@ -0,0 +1,208 @@
+from app import oidc
+from exceptions import KeycloakUserNotFound
+from tests.utils import dict_contains
+from unittest.mock import patch
+import tests.foodata as foodata
+from werkzeug.exceptions import Forbidden
+from models import RoleEnum, User
+import pytest
+
+
+class TestCapsuleOwners:
+
+    _owners_input = {
+        "newOwner": "tutu3",
+    }
+    _bad_owner_input = {
+        "owner": "titi4",
+    }
+
+    _owners_output = [ {"name": u} for u in foodata.capsule1["owners"] ]
+
+    _foobar = User(name="toto1", role=RoleEnum.user)
+    _fake_admin = User(name="fake_user", role=RoleEnum.admin)
+    _fake_superadmin = User(name="fake_user", role=RoleEnum.superadmin)
+    _fake_user = User(name="fake_user", role=RoleEnum.user)
+
+    _bad_capsule_id = "XYZ"
+    _unexisting_capsule_id = "ffffffff-ffff-ffff-ffff-ffffffffffff"
+
+    # Before anything we need a capsule id
+    def get_capsule_id(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            # Get the capsule id
+            res = testapp.get("/v1/capsules").json
+            return res[0]["id"]
+
+    ################################################
+    #### Testing GET /capsules/{cId}/owners
+    ################################################
+    # Response 400:
+    def test_get_bad_request(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.get("/v1/capsules/" + self._bad_capsule_id + "/owners", status=400)
+
+    # Response 401:
+    def test_get_with_no_token(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        testapp.get("/v1/capsules/" + capsule_id + "/owners", status=401)
+
+    # Response 403:
+    def test_get_raise_bad_owner(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._fake_user):
+
+            res = testapp.get("/v1/capsules/" + capsule_id + "/owners", status=403).json
+            assert "You don't have the permission to access the requested resource." in res["detail"]
+
+    # Response 404:
+    def test_get_bad_capsule_id(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            res = testapp.get("/v1/capsules/" + self._unexisting_capsule_id + "/owners", status=404).json
+            assert "The requested capsule '" + self._unexisting_capsule_id + "' has not been found." in res["detail"]
+
+    # Response 200:
+    def test_get(self, testapp, db):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            res = testapp.get("/v1/capsules/" + capsule_id + "/owners", status=200).json
+            assert dict_contains(res, self._owners_output)
+
+    ################################################
+
+    ################################################
+    #### Testing PATCH /capsules/{cId}/owners
+    ################################################
+    # Response 400:
+    def test_patch_bad_request_wrong_capsule_id(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+            testapp.patch_json("/v1/capsules/" + self._bad_capsule_id + "/owners", self._owners_input, status=400)
+
+    def test_patch_bad_request_wrong_input(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            res = testapp.patch_json("/v1/capsules/" + capsule_id + "/owners", self._bad_owner_input, status=400).json
+            print(res)
+            assert "The key newOwner is required." in res["detail"]
+
+    # Response 401:
+    def test_patch_unauthorized(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        testapp.patch_json("/v1/capsules/" + capsule_id + "/owners", self._owners_input, status=401)
+
+    # Response 403:
+    def test_patch_forbidden(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._fake_user):
+
+            res = testapp.patch_json("/v1/capsules/" + capsule_id + "/owners", self._owners_input, status=403).json
+            assert "You don't have the permission to access the requested resource." in res["detail"]
+
+    # Response 404:
+    def test_patch_not_found_capsule_id(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.patch_json("/v1/capsules/" + self._unexisting_capsule_id + "/owners", self._owners_input, status=404)
+
+    def test_patch_not_found_owner(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar), \
+            patch("api.capsules.owners.check_owners_on_keycloak", side_effect=KeycloakUserNotFound("tutu3")):
+
+            testapp.patch_json("/v1/capsules/" + capsule_id + "/owners", self._owners_input, status=404)
+
+    # Response 409: TODO => is it a real conflict ?
+    def test_patch_conflict(self,testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            new_owner = {
+                "newOwner": self._owners_output[0]["name"]
+            }
+
+            testapp.patch_json("/v1/capsules/" + capsule_id + "/owners", new_owner, status=409)
+
+    # Response 200:
+    def test_patch(self,testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            res = testapp.patch_json("/v1/capsules/" + capsule_id + "/owners", self._owners_input, status=200).json
+            assert dict_contains(res.owners, self._owners_input["name"])
+
+    ################################################
+
+    ################################################
+    #### Testing DELETE /capsules/{cId}/owners/uId
+    ################################################
+    # Response 400:
+    def test_delete_bad_request_wrong_capsule_id(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.delete("/v1/capsules/" + self._bad_capsule_id + "/owners/whatever", status=400)
+
+    # Response 401:
+    def test_delete_unauthorized(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        testapp.delete("/v1/capsules/" + capsule_id + "/owners/" + self._owners_output[0]["name"], status=401)
+
+    # Response 403:
+    def test_delete_forbidden(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._fake_user):
+
+            res = testapp.delete("/v1/capsules/" + capsule_id + "/owners/" + self._owners_output[0]["name"], status=403).json
+            assert "You don't have the permission to access the requested resource." in res["detail"]
+
+    # Response 404:
+    def test_delete_not_found_capsule_id(self, testapp):
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.delete("/v1/capsules/" + self._unexisting_capsule_id + "/owners/" + self._owners_output[0]["name"], status=404)
+
+    def test_delete_not_found_owner(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.delete("/v1/capsules/" + capsule_id + "/owners/" + self._fake_user.name, status=404)
+
+    # Response 409:
+    def test_delete_conflict(self,testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.delete("/v1/capsules/" + capsule_id + "/owners/" + self._foobar.name, status=409)
+
+    # Response 204:
+    @pytest.mark.filterwarnings("ignore:.*Content-Type header found in a 204 response.*:Warning")
+    def test_delete(self, testapp):
+        capsule_id = TestCapsuleOwners.get_capsule_id(self, testapp)
+        with patch.object(oidc, "validate_token", return_value=True), \
+            patch("utils.check_user_role", return_value=self._foobar):
+
+            testapp.delete("/v1/capsules/" + capsule_id + "/owners/" + self._owners_output[1]["name"], status=204)
+
+    ################################################
