781ccdf46698dfe0c9d92d19ac54fe927022af71
François Lafont <francois.lafont@ac-versailles.fr>
2020-06-10 18:19:02 +0200

    [wip]  Implements the class NATSDriverMsg
    Co-authored-by: gbarre <gerrab@gmail.com>

--- nats/listener.py
+++ nats/listener.py
@@ -7,6 +7,7 @@ from models import capsule_output_schema
 from models import webapp_schema
 from sqlalchemy import orm, create_engine
 from app import nats
+from json.decoder import JSONDecodeError
 
 
 # TODO: Configuration must be unified
@@ -31,38 +32,51 @@ class NATSListener(threading.Thread):
 
     @staticmethod
     def listen(msg):
+
+        msg = NATSDriverMsg(msg)
+
         # TODO: implements nats listening protocol
         origin_subject = msg.subject
-        payload = json.loads(msg.payload)
+        #payload = json.loads(msg.payload)
+        json_msg = msg.json
 
-        if 'state' not in payload:
-            nats.publish_error(origin_subject, 'invalid', 'state is missing.')
+        is_json_valid, error_msg = msg.is_json_valid()
+
+        if not is_json_valid:
+            nats.publish_error(
+                msg.subject,
+                'invalid',
+                error_msg)
             return
 
-        reqtype, obj = payload['state'].split(':', 1)
-        if reqtype == 'request':
-            if 'id' not in payload:
-                nats.publish_error(origin_subject, 'invalid', 'id is missing.')
-                return
+        # if 'state' not in json_msg:
+        #     nats.publish_error(origin_subject, 'invalid', 'state is missing.')
+        #     return
 
-            id = payload['id']
-            try:
-                if obj == 'capsule':
-                    capsule = session.query(Capsule).get(id)
-                    capsule_data = capsule_output_schema.dump(capsule).data
-                    nats.publish(origin_subject, capsule_data)
-                    return
-                elif obj == 'webapp':
-                    webapp = session.query(WebApp).get(id)
-                    webapp_data = webapp_schema.dump(webapp).data
-                    nats.publish(origin_subject, webapp_data)
-                    return
-            except Exception:
-                nats.publish_error(
-                    origin_subject,
-                    'not found',
-                    f'{obj} {id} has not been found.')
-                return
+        # reqtype, obj = json_msg['state'].split(':', 1)
+        # if reqtype == 'request':
+        #     if 'id' not in json_msg:
+        #         nats.publish_error(origin_subject, 'invalid', 'id is missing.')
+        #         return
+
+        #     id = json_msg['id']
+        #     try:
+        #         if obj == 'capsule':
+        #             capsule = session.query(Capsule).get(id)
+        #             capsule_data = capsule_output_schema.dump(capsule).data
+        #             nats.publish(origin_subject, capsule_data)
+        #             return
+        #         elif obj == 'webapp':
+        #             webapp = session.query(WebApp).get(id)
+        #             webapp_data = webapp_schema.dump(webapp).data
+        #             nats.publish(origin_subject, webapp_data)
+        #             return
+        #     except Exception:
+        #         nats.publish_error(
+        #             origin_subject,
+        #             'not found',
+        #             f'{obj} {id} has not been found.')
+        #         return
 
         nats.publish_error(origin_subject, 'invalid', 'nothing to be done.')
 
@@ -70,6 +84,39 @@ class NATSListener(threading.Thread):
         nats.logger.info('NATS listener waiting for incoming messages.')
         nats.client.wait()
 
+
+class NATSDriverMsg:
+
+    required_fields = [
+        'from',
+        'to',
+        'message',
+        'data',
+        'timestamp',
+        'signature'
+    ]
+
+    def __init__(self, nats_msg):
+        self.subject = nats_msg.subject
+        self.payload = nats_msg.payload
+        try:
+            self.json = json.loads(self.payload)
+        except JSONDecodeError:
+            self.json = None
+
+    def is_json_valid(self):
+        if self.json is None:
+            return (False, 'Invalid JSON structure')
+        if not isinstance(self.json, dict):
+            return (False, 'JSON must be an object')
+        for field in __class__.required_fields:
+            if field not in self.json:
+                return (False, f'Key "{field}" is required in JSON')
+        if not(isinstance(self.json['data'], dict) \
+           and 'capsule-id' in self.json['data']):
+            return (False, 'Data value must be an object '\
+                            'with the key "capsule-id"')
+        return (True, None)
 
 def create_nats_listener(app):
     nats.init_app(app)
