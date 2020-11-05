bb7fb7d4299890c6122b469468e552c816e79997
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2020-11-05 09:00:26 +0100

    [WIP] Investigate on ci crash

---
+++ prepare-ci-env.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PY_VERSION=${1:-3.6}
+
+DEBIAN_FRONTEND=noninteractive apt-get update -qy
+DEBIAN_FRONTEND=noninteractive apt-get install -y python${PY_VERSION} python3-pip python3-venv
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip install -r requirements.txt -r test-requirements.txt
