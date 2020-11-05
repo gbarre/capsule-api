102e2286d50c22198228571559f88a90daeaa0a4
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2020-11-05 09:10:04 +0100

    [WIP] sourcing venv

--- prepare-ci-env.bash
+++ prepare-ci-env.bash
@@ -2,6 +2,8 @@
 
 PY_VERSION=${1:-3.6}
 
+echo "Preparing this jobs with python${PY_VERSION}..."
+
 DEBIAN_FRONTEND=noninteractive apt-get update -qy
 DEBIAN_FRONTEND=noninteractive apt-get install -y python${PY_VERSION} python3-pip python3-venv
 python3 -m venv venv
